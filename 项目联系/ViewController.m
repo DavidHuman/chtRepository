a94cee8ffd758e37b833bb19fd0ee4844dc26b6d
chihaitao <cht_net@126.com>
2017-08-01 23:13:15 +0800

    1.I wrote a simple algorithm

--- 项目联系/ViewController.m
+++ 项目联系/ViewController.m
@@ -9,13 +9,20 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic) int sum;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    int a = 5;
+    int b = 10;
+    self.sum = a + b;
+    NSLog(@"The result is : %d",self.sum);
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
